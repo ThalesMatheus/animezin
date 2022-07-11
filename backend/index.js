ef46b2cbb765a38fed11972261ce8dccc49d5e5d
JoaoVitorOliMendes <joaozao.oli@gmail.com>
2022-07-11 19:25:35 -0300

    boilerplate author

--- backend/index.js
+++ backend/index.js
@@ -3,3 +3,6 @@ app = require('./src/index')
 app.listen(3000, () => {
     console.info("Aplicação em http://localhost:3000")
 })
+
+
+// APPLICATION BOILERPLATE - https://github.com/hagopj13/node-express-boilerplate
