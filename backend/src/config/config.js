fb67a7dc716830bd8adab5f8cbb371147e216a57
JoaoVitorOliMendes <joaozao.oli@gmail.com>
2022-07-16 14:01:58 -0300

    new regex patterns

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -1,7 +1,14 @@
 const BASE_URL = `https://gogoanime.lu/`
 const EP_PATTERN = /.+ep_end.+>(?:\d+-)?(\d+)<\/a>/ig
 const TITLE_PATTERN = /<p class=["']name["']><a href=["']\/category\/(.+)["'] title=["'](.+)["']>/ig
-const EPISODE_BANNER = /<a href=["'](.+)["'] title=["'](.+)["']>(\r\n.+|\r.+|\n.+)<img src=["'](.+)["'] alt=["'](.+)["']/ig
+const EPISODE_BANNER = /<a href=["']\/(.+)["'] title=["'](.+)["']>(?:\r\n.+|\r.+|\n.+)<img src=["'](.+)["'] alt=["'](?:.+)["']/ig
+const INFO_BANNER_PATTERN = /<div class=["']anime_info_body_bg["']>(?:\r\n.+|\r.+|\n.+)<img src=["'](.+)["']/g
+const INFO_TYPE_PATTERN = /<span>Type: <\/span>(?:\r\n.+|\r.+|\n.+)<a href=["']\/sub-category\/(.+)["'] title=["'](.+)["']/g
+const INFO_GENRE_PATTERN = /<a href=["']https:\/\/gogoanime.lu\/genre\/([a-zA-Z0-9-]+)["'] title=["']([a-zA-Z0-9\s]+)["']/ig
+const INFO_RELEASED_PATTERN = /<span>Released: <\/span>(.+)<\/p>/ig
+const INFO_STATUS_PATTERN = /<span>Status: <\/span>(?:\r\n.+|\r.+|\n.+)<a href=["']\/(?:.+)["'] title=["'](?:.+)["']>(.+)<\/a>/ig
+const INFO_OTHERNAME_PATTERN = /<span>Other name: <\/span>(.+)<\/p>/ig
+const DESCRIPTION_PATTERN = /<span>Plot Summary: <\/span>(.+)/g
 const REFERER_PATTERN = /<a href=["']#["'] rel=["'](?:1|100)["'] data-video=["'](.+)["'] >/ig
 
 const VIDEO_PATTERN = /sources:\[{file: ["'](.+\.m3u8)["']/ig
@@ -12,5 +19,12 @@ module.exports = {
     TITLE_PATTERN,
     EPISODE_BANNER,
     REFERER_PATTERN,
-    VIDEO_PATTERN
+    INFO_BANNER_PATTERN,
+    INFO_TYPE_PATTERN,
+    INFO_GENRE_PATTERN,
+    VIDEO_PATTERN,
+    DESCRIPTION_PATTERN,
+    INFO_RELEASED_PATTERN,
+    INFO_STATUS_PATTERN,
+    INFO_OTHERNAME_PATTERN
 }
