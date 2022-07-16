d1e86542750fb85c8c5938c65cfc1986cb1a8f5a
JoaoVitorOliMendes <joaozao.oli@gmail.com>
2022-07-16 14:46:16 -0300

    fix

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -3,13 +3,13 @@ const EP_PATTERN = /.+ep_end.+>(?:\d+-)?(\d+)<\/a>/ig
 const TITLE_PATTERN = /<p class=["']name["']><a href=["']\/category\/(.+)["'] title=["'](.+)["']>/ig
 const EPISODE_BANNER = /<a href=["']\/(.+)["'] title=["'](.+)["']>(?:\r\n.+|\r.+|\n.+)<img src=["'](.+)["'] alt=["'](?:.+)["']/ig
 const INFO_DIV_PATTERN = /<div class=["']anime_info_body_bg["']>(.|\n)*?<\/div>/ig
-const INFO_BANNER_PATTERN = /<div class=["']anime_info_body_bg["']>(?:\r\n.+|\r.+|\n.+)<img src=["'](.+)["']/g
-const INFO_TYPE_PATTERN = /<span>Type: <\/span>(?:\r\n.+|\r.+|\n.+)<a href=["']\/sub-category\/(.+)["'] title=["'](.+)["']/g
+const INFO_BANNER_PATTERN = /<img src=["'](.+)["'] (?:alt=["'].+["'])/ig
+const INFO_TYPE_PATTERN = /<span>Type: <\/span>(?:\r\n.+|\r.+|\n.+)<a href=["']\/sub-category\/(.+)["'] title=["'](.+)["']/ig
 const INFO_GENRE_PATTERN = /<a href=["']https:\/\/gogoanime.lu\/genre\/([a-zA-Z0-9-]+)["'] title=["']([a-zA-Z0-9\s]+)["']/ig
 const INFO_RELEASED_PATTERN = /<span>Released: <\/span>(.+)<\/p>/ig
 const INFO_STATUS_PATTERN = /<span>Status: <\/span>(?:\r\n.+|\r.+|\n.+)<a href=["']\/(?:.+)["'] title=["'](?:.+)["']>(.+)<\/a>/ig
 const INFO_OTHERNAME_PATTERN = /<span>Other name: <\/span>(.+)<\/p>/ig
-const DESCRIPTION_PATTERN = /<span>Plot Summary: <\/span>(.+)/g
+const DESCRIPTION_PATTERN = /<span>Plot Summary: <\/span>(.+)[<\/p>]*/g
 const REFERER_PATTERN = /<a href=["']#["'] rel=["'](?:1|100)["'] data-video=["'](.+)["'] >/ig
 
 const VIDEO_PATTERN = /sources:\[{file: ["'](.+\.m3u8)["']/ig
